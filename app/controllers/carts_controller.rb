92fd1b91217ad382445ce2ca7d46f44e310a4e09
Stuart Claghorn <sclaghorn@gmail.com>
2014-07-04 13:06:27 -0400

    Error cart handling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,6 +10,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+      begin
+        @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_url, notice: 'Invalid cart'
+      else
+        respond_to do |format|
+          format.html # show.html.erb
+          format.json { render jscon: @cart }
+        end
+      end
   end
 
   # GET /carts/new
@@ -64,7 +75,17 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+        begin
+          @cart = Cart.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          logger.error "Attempt to access invalid cart #{params[:id]}"
+          redirect_to store_url, notice: 'Invalid cart'
+        else
+          respond_to do |format|
+            format.html # show.html.erb
+            format.json { render jscon: @cart }
+        end
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

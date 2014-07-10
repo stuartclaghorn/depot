1881a6ce73f5a5619e4845d16901256eecc2b3a9
Stuart Claghorn <sclaghorn@gmail.com>
2014-07-10 10:14:18 -0400

    Fixed paginate for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-	@orders = Order.paginate page: params[:page], order: 'created_at desc',
-		per_page: 10
+	@orders = Order.order('created_at DESC').paginate(page: params[:page], per_page: 10)
 
 	respond_to do |format|
 		format.html # index.html.erb

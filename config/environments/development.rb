d82457fcef907bb873971729ccb8655386bfdbf0
Stuart Claghorn <sclaghorn@gmail.com>
2014-07-11 15:52:23 -0400

    Integration tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,20 @@ Depot::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+	  # address: "smtp.gmail.com",
+	  # port: 587,
+	  # domain: "domain.of.sender.net",
+	  # authentication: "plain",
+	  # user_name: "dave",
+	  # password: "secret",
+	  # enable_starttls_auto: true
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,4 +40,5 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,16 @@ Depot::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+	  # address: "smtp.gmail.com",
+	  # port: 587,
+	  # domain: "domain.of.sender.net",
+	  # authentication: "plain",
+	  # user_name: "dave",
+	  # password: "secret",
+	  # enable_starttls_auto: true
+  # }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr

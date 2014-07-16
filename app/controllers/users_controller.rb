71f38cbb774c8dee119b37714fc47b224652ce1e
Stuart Claghorn <sclaghorn@gmail.com>
2014-07-15 23:47:12 -0400

    Users cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(params[:user])
 
     respond_to do |format|
       if @user.save
